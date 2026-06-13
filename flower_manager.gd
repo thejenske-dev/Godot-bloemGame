7cbc49e621eac91ae22652699b061c762552ac86
Jens V <thejenske@gmail.com>
2026-06-13 16:32:03 +0200

    Added colour change w/ self modulate when pressing down and up arrows

--- flower_manager.gd
+++ flower_manager.gd
@@ -2,6 +2,10 @@ extends Node2D
 var flower = preload("res://Scenes/flower.tscn")
 var m_pos:Vector2
 var reset_active:bool = false
+var flowercolours_pos = 0
+var flowercolours = [0,0,0]
+var flowerColor : Color
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,8 +18,12 @@ func _process(delta: float) -> void:
 		print(m_pos)
 		var flower_pos = Vector2(get_global_mouse_position())
 		new_flower.position = flower_pos
+		new_flower.changeColor(flowerColor)
 		add_child(new_flower)
-	
+		
+		
+		
+	#Resetting the garden and deletes all child flowers
 	if(Input.is_action_just_pressed("Reset") and reset_active == false ):
 		reset_active = true
 		var delay=.1
@@ -28,4 +36,26 @@ func _process(delta: float) -> void:
 			reset_active = false 
 		else:
 			print("No flowers :)")
-			
+	
+	
+	#Changing flower colours
+	if(Input.is_action_just_pressed("arrow_up")):
+		#Change flower colour sprite
+		if(flowercolours_pos <2):
+			flowercolours_pos +=1
+			flowercolours[flowercolours_pos] = 1
+		else:
+			flowercolours_pos =0
+			flowercolours[flowercolours_pos] = 1
+		flowerColor = Color(flowercolours[0],flowercolours[1],flowercolours[2])
+		print("arrow_up pressed")
+	if(Input.is_action_just_pressed("arrow_down")):
+		#Change flower colour sprite
+		if(flowercolours_pos >0):
+			flowercolours_pos -=1
+			flowercolours[flowercolours_pos] = 0
+		else:
+			flowercolours_pos =2
+			flowercolours[flowercolours_pos] = 0
+		flowerColor = Color(flowercolours[0],flowercolours[1],flowercolours[2])
+		print("arrow_down pressed")		
